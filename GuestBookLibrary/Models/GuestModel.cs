a85a00129138ac3dcece52aaa1b31cd2b73f61ff
aaladdin <aladdin.frontend@gmail.com>
2024-03-22 20:56:38 +0200

    Completed Guestbook

---
+++ GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuestBookLibrary.Models {
+    public class GuestModel {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MessageToHost { get; set; }
+
+        public string GuestInfo {
+            get {
+                return $"{FirstName} {LastName} says: {MessageToHost}";
+            }
+        }
+    }
+}
--- GuestBookUI/Program.cs
+++ GuestBookUI/Program.cs
@@ -1,9 +1,46 @@
-﻿using System;
+﻿using GuestBookLibrary.Models;
+using System;
+using System.Collections.Generic;
 
 namespace GuestBookUI {
-    internal class Program {
+    class Program {
+
+        private static List<GuestModel> guests = new List<GuestModel>();
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to the Guest Book!");
+
+            CollectGuestInfo();
+            PrintGuests();
+        }
+
+        static void CollectGuestInfo() {
+            string moreGuests = "";
+            do
+            {
+                GuestModel guest = new GuestModel();
+                guest.FirstName =  GetInfo("Please enter First Name: ");
+                guest.LastName = GetInfo("Please enter Last Name: ");
+                guest.MessageToHost = GetInfo("Please enter Message To Host: ");
+
+                guests.Add(guest);
+
+                moreGuests = GetInfo("Do you want to add another guest? (yes/no)");
+            } while (moreGuests.ToLower() == "yes");
+        }
+
+        static string GetInfo(string message) {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+
+        static void PrintGuests() {
+            foreach (var guest in guests)
+            {
+                Console.WriteLine(guest.GuestInfo);
+            }
         }
     }
 }
