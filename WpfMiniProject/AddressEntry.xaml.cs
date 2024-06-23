07ea5a9f870cbe1aad4913f428e465508aba9554
aaladdin <aaladdin@finboa.com>
2024-06-23 21:20:28 +0300

    Implemented WPF Mini Project

---
+++ WpfMiniProject/AddressEntry.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfMiniProject
+{
+    /// <summary>
+    /// Interaction logic for AddressEntry.xaml
+    /// </summary>
+    public partial class AddressEntry : Window
+    {
+        ISaveAddress _parent;
+        public AddressEntry(ISaveAddress parent)
+        {
+            InitializeComponent();
+            _parent = parent;   // saving the parent object for long term use.
+        }
+       
+    }
+}
---
+++ WpfMiniProject/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMiniProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<AddressModel> addresses = new BindingList<AddressModel>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            adressesList.ItemsSource = addresses;
+            addresses.Add(new AddressModel { StreetAddress = "123 Main St", City = "Columbus", State = "Ohio", ZipCode = "43215" });
+            addresses.Add(new AddressModel {  StreetAddress = "456 Elm St", City = "Columbus2", State = "Ohio2", ZipCode = "8910" });
+        }
+    }
+}
