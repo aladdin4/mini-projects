1077075b0c9573f52dff55b159b5c83fa668faaa
aaladdin <aaladdin@finboa.com>
2024-06-27 12:47:53 +0300

    Implemented API Endpoints

---
+++ ApiMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,27 @@
+﻿using DemoLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+     
+
+        //POST api/Address
+        public void Post([FromBody] AddressModel address)
+        {
+            _logger.LogInformation("Address {0} {1} {2} {3} added", address.StreetAddress, address.City, address.State, address.ZipCode);
+        }
+    }
+}
---
+++ ApiMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,31 @@
+﻿using DemoLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiMiniProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+
+        }
+        // POST api/Person
+        [HttpPost]
+        public void Post([FromBody] PersonModel person)
+        {
+            _logger.LogInformation("Person {0} {1} added", person.FirstName, person.LastName);
+        }
+
+      
+
+    }
+}
