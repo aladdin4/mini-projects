edcb50a9a9e54a4bda73463c40c007bd9d709665
aaladdin <aaladdin@finboa.com>
2025-06-08 07:37:12 +0300

    Scaffolding the Model and Controller. Created SP

--- TodoApp/TodoApi/Controllers/TodosController.cs
+++ TodoApp/TodoApi/Controllers/TodosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoLibrary.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,40 +11,44 @@ namespace TodoApi.Controllers
     {
         // GET: api/Todos
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<IEnumerable<TodoModel>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok();
         }
 
         // GET api/Todos/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<TodoModel> Get(int id)
         {
-            return "value";
+            return Ok();
         }
 
         // POST api/Todos
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] string value)
         {
+            return Ok();
         }
 
         // PUT api/Todos/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] string value)
         {
+            return Ok();
         }
 
         // PUT api/Todos/5/complete
         [HttpPut("{id}/complete")]
-        public void Complete(int id)
+        public IActionResult Complete(int id)
         {
+            return Ok();
         }
 
         // DELETE api/Todos/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            return Ok();
         }
     }
 }
