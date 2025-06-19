098e0cddaeed533fb7595004c8c0d0c67ba06b20
A. Aladdin <aladdin.FullStack@gmail.com>
2025-06-19 17:25:03 +0300

    Implemented DataAccess Layer (Library)

--- TodoApp/TodoLibrary/DataAccess/SqlDataAccess.cs
+++ TodoApp/TodoLibrary/DataAccess/SqlDataAccess.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TodoLibrary.DataAccess
+namespace TodoLibrary.DataAccess;
+
+public class SqlDataAccess : ISqlDataAccess
 {
-    class SqlDataAccess
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
     {
+
+        _config = config;
+    }
+
+    public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+        using IDbConnection connection = new SqlConnection(connectionString);
+        List<T> rows = (await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure)).ToList();
+        return rows;
+    }
+
+    //returning the task, hence, the execution will be Sync. And the waiting is expected to be done by the caller
+    public Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+        using IDbConnection connection = new SqlConnection(connectionString);
+        return connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
     }
 }
---
+++ TodoApp/TodoLibrary/DataAccess/TodoData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoLibrary.Models;
+
+namespace TodoLibrary.DataAccess;
+
+public class TodoData : ITodoData
+{
+    private readonly ISqlDataAccess _sql;
+    public TodoData(ISqlDataAccess sql)      //we must register this in the DI to receive this one, right??
+                                             //Yes, but first we need to create the interface
+    {
+        _sql = sql;
+    }
+
+    public Task<List<TodoModel>> GetAllAssigned(int assignedTo)
+    {
+        return _sql.LoadData<TodoModel, dynamic>("dbo.spTodos_GetAllAssigned", new { AssignedTo = assignedTo }, "Default");
+    }
+
+    public async Task<TodoModel?> GetOneAssigned(int assignedTo, int todoId)
+    {
+        var result = await _sql.LoadData<TodoModel, dynamic>("dbo.spTodos_GetOneAssigned", new { AssignedTo = assignedTo, TodoId = todoId }, "Default");
+        return result.FirstOrDefault();
+    }
+
+    public async Task<TodoModel?> Create(int assignedTo, string task)
+    {
+        var result = await _sql.LoadData<TodoModel, dynamic>("dbo.spTodos_Create", new { Task = task, AsssignedTo = assignedTo }, "Default");
+        return result.FirstOrDefault();
+    }
+
+    public Task UpdateTask(string Task, int assignedTo, int todoId)
+    {
+        return _sql.SaveData<dynamic>("dbo.spTodos_UpdateTask", new { Task = Task, AssignedTo = assignedTo, TodoId = todoId }, "Default");
+    }
+
+    public Task CompleteTodo(int assignedTo, int todoId)
+    {
+        return _sql.SaveData<dynamic>("dbo.spTodos_CompleteTodo", new { AssignedTo = assignedTo, TodoId = todoId }, "Default");
+    }
+
+    public Task DeleteTodo(int assignedTo, int todoId)
+    {
+        return _sql.SaveData<dynamic>("dbo.spTodos_Delete", new { TodoId = todoId, AssignedTo = assignedTo }, "Default");
+    }
+
+}
