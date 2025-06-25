8f10c3fd44ca023daa8a29f0b96fe3fcfed6ce91
A. Aladdin <aladdin.fullstack@gmail.com>
2025-06-25 15:43:17 +0300

    Implemented Logging to the API

--- TodoApp/TodoApi/Controllers/TodosController.cs
+++ TodoApp/TodoApi/Controllers/TodosController.cs
@@ -13,63 +13,127 @@ namespace TodoApi.Controllers
     {
 
         private readonly ITodoData _data;
+        private readonly ILogger<TodosController> _logger;
 
-        public TodosController(ITodoData data)
+        public TodosController(ITodoData data, ILogger<TodosController> logger)
         {
             _data = data;
+            _logger = logger;
         }
 
         private int GetUserId()
         {
-             var userText = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            var userText = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
             return int.Parse(userText!);
         }
         // GET: api/Todos
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoModel>>> Get()
         {
-            var result = await _data.GetAllAssigned(GetUserId());
-            return Ok(result); // we like to be explicit.
+            _logger.LogInformation("GET: api/Todos - Start");
+            try
+            {
+                var result = await _data.GetAllAssigned(GetUserId());
+                _logger.LogInformation("GET: api/Todos - Success");
+                return Ok(result); // we like to be explicit.
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "GET: api/Todos - Failed");
+                return BadRequest();
+            }
         }
 
-        // GET api/Todos/5
+        // GET: api/Todos/5
         [HttpGet("{todoId}")]
         public async Task<ActionResult<TodoModel>> Get(int todoId)
         {
-            var result = await _data.GetOneAssigned(GetUserId(), todoId);
-            return Ok(result);
+            _logger.LogInformation($"GET: api/Todos/{todoId} - Start");
+            try
+            {
+                var result = await _data.GetOneAssigned(GetUserId(), todoId);
+                _logger.LogInformation($"GET: api/Todos/{todoId} - Success");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"GET: api/Todos/{todoId} - Failed");
+                return BadRequest();
+            }
         }
 
-        // POST api/Todos
+        // POST: api/Todos
         [HttpPost]
         public async Task<ActionResult<TodoModel>> Post([FromBody] string task)
         {
-            var result = await _data.Create(GetUserId(), task);
-            return Ok(result);
+            _logger.LogInformation($"POST: api/Todos - Start");
+            _logger.LogInformation($"Adding: {task}");
+            try
+            {
+                var result = await _data.Create(GetUserId(), task);
+                _logger.LogInformation($"POST: api/Todos - Success");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"POST: api/Todos - Failed");
+                return BadRequest();
+            }
         }
 
-        // PUT api/Todos/5
+        // PUT: api/Todos/5
         [HttpPut("{todoId}")]
         public async Task<ActionResult> Put(int todoId, [FromBody] string task)
         {
-            await _data.UpdateTask(task, GetUserId(), todoId);
-            return Ok();
+            _logger.LogInformation($"PUT: api/Todos/{todoId} - Start");
+            _logger.LogInformation($"Updating With: {task}");
+            try
+            {
+                await _data.UpdateTask(task, GetUserId(), todoId);
+                _logger.LogInformation($"PUT: api/Todos/{todoId} - Success");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"PUT: api/Todos/{todoId} - Failed");
+                return BadRequest();
+            }
         }
 
-        // PUT api/Todos/5/complete
+        // PUT: api/Todos/5/complete
         [HttpPut("{todoId}/complete")]
         public async Task<IActionResult>  Complete(int todoId)
         {
-            await _data.CompleteTodo(GetUserId(), todoId);
-            return Ok();
+            _logger.LogInformation($"PUT: api/Todos/{todoId}/complete - Start");
+            try
+            {
+                await _data.CompleteTodo(GetUserId(), todoId);
+                _logger.LogInformation($"PUT: api/Todos/{todoId}/complete - Success");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"PUT: api/Todos/{todoId}/complete - Failed");
+                return BadRequest();
+            }
         }
 
-        // DELETE api/Todos/5
+        // DELETE: api/Todos/5
         [HttpDelete("{todoId}")]
         public async Task<IActionResult> Delete(int todoId)
         {
-            await _data.DeleteTodo(GetUserId(), todoId);
-            return Ok();
+            _logger.LogInformation($"DELETE: api/Todos/{todoId} - Start");
+            try
+            {
+                await _data.DeleteTodo(GetUserId(), todoId);
+                _logger.LogInformation($"DELETE: api/Todos/{todoId} - Success");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"DELETE: api/Todos/{todoId} - Failed");
+                return BadRequest();
+            }
         }
     }
 }
